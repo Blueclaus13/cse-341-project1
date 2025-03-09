ce0ae5f6923b176ffcb265df1416013557e6844a
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2025-03-08 22:30:37 -0600

    added MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => {
-    console.log('Web Server is listening at port ' + (PORT));
-  });
+mongodb.initDb((err)=>{
+  if(err){
+    console.log("There is an error: "+ err);
+  }else{
+    app.listen(PORT, () => {
+      console.log('Web Server is listening at port ' + (PORT));
+    });
+  }
+});
