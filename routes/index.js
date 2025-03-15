9f4fbbdbee697ffff8b72f1094a7fa6fce15e32d
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2025-03-14 22:48:24 -0600

    Added swagger docs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 // router.get('/', (req, res)=>{
 //     res.send('Hello Wold');
 // });
-
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
